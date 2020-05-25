08c8b2ef14ab97bb913269109707264cc7d83eae
Lucy Toman <lucy@toman.me.uk>
2020-05-25 17:12:22 +0200

    renders first question on page

--- src/questions.js
+++ src/questions.js
@@ -9,33 +9,50 @@ export default function Questions() {
         axios
             .get(`/testquestions`)
             .then((response) => {
-                // console.log("/testquestions response", response.data.results.rows);
+                // console.log(
+                //     "/testquestions response",
+                //     response.data.results.rows
+                // );
 
                 setTestQuestions(response.data.results.rows); //array of 10 objects
-
-                console.log("test questions", testquestions);
             })
             .catch((err) => {
                 console.log("error in settestqs", err);
             });
     }, []);
+    console.log("test questions", testquestions);
+
+    let question = testquestions[0];
+
+    //method called by next button, iterates through testquestions and returns next
 
     return (
         <div className="question-container">
-            <div className="German">
-                {/* {testquestions[0].first} {"___"}{" "}
-                {testquestions[0].last} */}
-            </div>
-            <div className="English">{/* {testquestions[0].trans} */}</div>
-            <div className="answer-container">
-                <textarea name="answer" placeholder="type answer here..." />
-                <button className="feedback">X or tick</button>
-            </div>
-            <div className="nav-buttons">
-                <button>Submit answer</button>
-                <button>Show answer</button>
-                <button>Next question</button>
-            </div>
+            {question && (
+                <div>
+                    <div className="German">
+                        <p>
+                            {" "}
+                            {question.first} {"___"} {question.second}
+                        </p>
+                    </div>
+                    <div className="English">
+                        <p>{question.trans}</p>
+                    </div>
+                    <div className="answer-container">
+                        <textarea
+                            name="answer"
+                            placeholder="type answer here..."
+                        />
+                        <button className="feedback">X or tick</button>
+                    </div>
+                    <div className="nav-buttons">
+                        <button>Submit answer</button>
+                        <button>Show answer</button>
+                        <button>Next question</button>
+                    </div>
+                </div>
+            )}
         </div>
     );
 }
