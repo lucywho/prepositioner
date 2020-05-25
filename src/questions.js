f1ce15d3a79861a6d828275dda35f45183437bb3
Lucy Toman <lucy@toman.me.uk>
2020-05-25 17:52:50 +0200

    adds function to iterate through question array on click, adds notes for further work

--- src/questions.js
+++ src/questions.js
@@ -25,20 +25,29 @@ export default function Questions() {
     let question = testquestions[0];
     let i = 1;
 
-    //method called by next button, iterates through testquestions and returns next
     function next() {
         console.log("clicked on next");
         question = testquestions[i];
         console.log("question after next", question);
         i++;
+
+        //TO DO: how to update render with new question?
     }
 
     function submit() {
         console.log("clicked on submit");
+        //TO DO: capture input, convert to lowercase
+        //compare with question.answer
+        //if correct, render tick in feedback (and update score)
+        // -- feedback needs conditional
+        // -- work out how to do score (see c4?)
+        // if incorrect, render x in feedback
     }
 
     function show() {
         console.log("clicked on show answer");
+        //TO DO: display question.first, question.answer, question.second
+        //decide where - hidden field or replace contents of German field?
     }
 
     return (
